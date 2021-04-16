3f77b743bde1e1c82b100b3d4db3570be5a6c4b6
Daisugi <volble124@gmail.com>
2021-04-16 01:21:47 -0700

    add asignment

--- AlgorithmsDataStructures/Assignments/Greedy/MinimumCostFlow.swift
+++ AlgorithmsDataStructures/Assignments/Greedy/MinimumCostFlow.swift
@@ -53,7 +53,7 @@ func minimumCostFlow(testMode: Bool = false, filePath: String = "") {
 
   // 3. make MST by kruskal Algorithm
   var days = 0
-  var uf = UF(graph.count)
+  var uf = UFForCF(graph.count)
   var set = Set<CompEdgeA>()
   var mstEdges = [CompEdgeA]()
   var maxCostEdge: CompEdgeA? = nil
@@ -116,7 +116,7 @@ func minimumCostFlow(testMode: Bool = false, filePath: String = "") {
 /// get input from given test cases
 /// - Parameter filePath: file path of test cases
 /// - Returns: graph and a number of buildings, pipes, and enhancerStrength
-private func getTestInput(from filePath: String) -> ([[CompEdgeA]], Int, Int, Int) {
+fileprivate func getTestInput(from filePath: String) -> ([[CompEdgeA]], Int, Int, Int) {
   let contents = try! String(contentsOfFile: filePath, encoding: .utf8)
   let lines = contents.split(separator: "\n")
 
@@ -145,7 +145,7 @@ private func getTestInput(from filePath: String) -> ([[CompEdgeA]], Int, Int, In
 ///   - days: output
 ///   - start: start time for calculate performance
 ///   - testMode: true: test mode, false: user input
-private func printResult(filePath: String, days: Int, start: Date, testMode: Bool) {
+fileprivate func printResult(filePath: String, days: Int, start: Date, testMode: Bool) {
   let elapsed = Date().timeIntervalSince(start)
   if testMode {
     var filePath = filePath[0, filePath.count - 2]
@@ -160,7 +160,7 @@ private func printResult(filePath: String, days: Int, start: Date, testMode: Boo
 }
 
 
-struct CompEdgeA {
+fileprivate struct CompEdgeA {
   let from: Int
   let to: Int
   let cost: Int
@@ -177,3 +177,69 @@ extension CompEdgeA: Equatable {
 }
 
 extension CompEdgeA: Hashable {}
+
+
+fileprivate struct UFForCF {
+  /// parent[i] = parent of i
+  private var parent: [Int]
+  /// size[i] = number of nodes in tree rooted at i
+  private var size: [Int]
+  /// number of components
+  private(set) var count: Int
+  
+  /// Initializes an empty union-find data structure with **n** elements
+  /// **0** through **n-1**.
+  /// Initially, each elements is in its own set.
+  /// - Parameter n: the number of elements
+  public init(_ n: Int) {
+    self.count = n
+    self.size = [Int](repeating: 1, count: n)
+    self.parent = [Int](repeating: 0, count: n)
+    for i in 0..<n {
+      self.parent[i] = i
+    }
+  }
+  
+  /// Returns the canonical element(root) of the set containing element `p`.
+  /// - Parameter p: an element
+  /// - Returns: the canonical element of the set containing `p`
+  public mutating func findRoot(of i: Int) -> Int {
+    var i = i
+    while i != parent[i] {
+      parent[i] = parent[parent[i]]
+      i = parent[i]
+    }
+    
+    return i
+  }
+  
+  /// Returns `true` if the two elements are in the same set.
+  /// - Parameters:
+  ///   - p: one elememt
+  ///   - q: the other element
+  /// - Returns: `true` if `p` and `q` are in the same set; `false` otherwise
+  public mutating func connected(_ p: Int, _ q: Int) -> Bool {
+    
+    return findRoot(of: p) == findRoot(of: q)
+  }
+  
+  /// Merges the set containing element `p` with the set containing
+  /// element `q`
+  /// - Parameters:
+  ///   - p: one element
+  ///   - q: the other element
+  @discardableResult
+  public mutating func union(_ p: Int, _ q: Int) -> Bool {
+    var i = findRoot(of: p)
+    var j = findRoot(of: q)
+    
+    if i == j { return false }
+
+    if size[i] >= size[j] { swap(&i, &j) }
+    
+    parent[i] = j
+    size[j] += size[i]
+    
+    return true
+  }
+}
